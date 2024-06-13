5dca4f2c3f77c9d2d39629b6c9fc97706d3eaf3e
mikosis123 <mikosis93@gmail.com>
2024-06-13 11:19:40 +0300

    pool manager  interface

--- IQoutationFeatch.sol
+++ IQoutationFeatch.sol
@@ -1,45 +1,14 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract IQoutationFeatch {
 
-    struct LiquidityPool {
-        uint256 reserve0;
-        uint256 reserve1;
-        uint256 lowerBound;
-        uint256 upperBound;
-        mapping(address => uint256) providerAmounts;
-    }
 
-    mapping(bytes32 => LiquidityPool) public liquidityPools;
+interface IAllPoolManager{
 
-    // Function to get the price quote from a liquidity pool
-    function getPriceQuote(bytes32 poolId) public view returns (uint256 price) {
-        LiquidityPool storage pool = liquidityPools[poolId];
-        require(pool.reserve0 > 0 && pool.reserve1 > 0, "Insufficient liquidity");
-
-        price = (pool.reserve1 * 1e18) / pool.reserve0; // Assuming 18 decimals for simplicity
-        require(price >= pool.lowerBound && price <= pool.upperBound, "Price out of range");
-    }
-
-    // Function to view the possible range for a quote
-    function getPriceRange(bytes32 poolId) public view returns (uint256 lowerBound, uint256 upperBound) {
-        LiquidityPool storage pool = liquidityPools[poolId];
-        return (pool.lowerBound, pool.upperBound);
-    }
-
-    // Function to get the details of a pool provider
-    function getProviderDetails(bytes32 poolId, address provider) public view returns (uint256 amountProvided) {
-        LiquidityPool storage pool = liquidityPools[poolId];
-        return pool.providerAmounts[provider];
-    }
-
-    // Function to fetch the latest price from Chainlink PriceFeeds
-    function getChainlinkPrice(address aggregator) public view returns (int256 price, uint8 decimals) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(aggregator);
-        (, price, , , ) = priceFeed.latestRoundData();
-        decimals = priceFeed.decimals();
-    }
+    function createPool(address inputToken, address outputToken,uint256 fee)external returns(address pool);
+    function addLiquidityToPool(address inputToken,address pool,uint256 amount) external;
+    function removeLiquidityFromPool(address outputToken,address pool,uint256 amount)external;
+    function collectReward(address pool)external;
+    function swap(address inputToken,address outputToken,uint256 amount) external;
 }
